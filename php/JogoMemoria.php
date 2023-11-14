352a2e4d538009bf667a4b58372e8258ed57adcd
LuisGabrielMascarenhas <luis.gabriel.santos614@gmail.com>
2023-11-13 22:02:17 -0300

    php JogoMemoria

---
+++ php/JogoMemoria.php
@@ -0,0 +1,20 @@
+<?php
+
+    class JogoMemoria{
+        private array $jogo;
+        private array $imagem = array(
+            'assets/abobora.png',
+            'assets/fantasma.png',
+        );    
+
+        public int $erros;
+        public int $acertos;
+        public string $html;
+        public string $errosHtml;
+
+        
+   
+    }
+
+
+?>
